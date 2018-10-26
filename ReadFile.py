73dd31bc3719c729532e44b7a4905daab847b532
RonElhar <ronelharrar@gmail.com>
2018-10-26 16:29:50 +0300

    Created function of returning sepreated documents contents list with tags for now)

--- ReadFile.py
+++ ReadFile.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 
 def createSeperatedDoc(path, filename, currentContent):
@@ -10,16 +11,47 @@ def seperateDocsInDir(path):
     for filename in os.listdir(path):
         curFile = open(path+"\\"+filename+"\\"+filename,"r")
         lines = curFile.readlines()
-        currentContent = ""
+        currentContent = []
         for line in lines:
-                currentContent.__add__(line)
-                if line ==  "</DOC>":
-                    docsContent.__add__(currentContent)
-                    currentContent = ""
-    return docsContent
+                currentContent.append(line)
+                if  line.__contains__("</DOC>"):
+                    docsContent.append(currentContent)
+                    return docsContent[0]
+                    #currentContent = ""
+    #print(docsContent[0])
+    #return docsContent
+
+def getText():
+    docText = ""
+    isText = False
+    docContent = seperateDocsInDir("C:\Users\USER\Desktop\SearchEngine\corpus")
+    for line in docContent:
+        if line.__contains__("</TEXT>"):
+            isText = False
+        if isText:
+            docText+=line
+        if line.__contains__("<TEXT>"):
+            isText = True
+    return docText
+
+def getTerms(text):
+    terms = str.split(text," ")
+    tCount=0
+    for term in terms:
+        terms[tCount] = re.sub('[^A-Za-z0-9\-$%/]+', '', term)
+        if terms[tCount].isdigit() == False and terms[tCount].__contains__('.') :
+            terms[tCount] = terms[tCount].replace('.', '')
+        if terms[tCount].isdigit() and terms[tCount].__contains__('.'):
+            print(terms[tCount])
+        terms[tCount] = terms[tCount].replace('\n','')
+        tCount+=1
+    terms = filter(None, terms)
+    return terms
+
+
+
 
                     
 
-
-#problem with permissions in corpus file, couldn't make it work
-seperateDocsInDir("C:\Users\USER\Desktop\SearchEngine\corpus")
+getTerms(getText())
+#seperateDocsInDir("C:\Users\USER\Desktop\SearchEngine\corpus")
