3665503a8d7c8e830777993101bafc69c0676ee9
RonElhar <ronelharrar@gmail.com>
2018-10-28 01:29:18 +0300

    fixed Get terms , now working with decimals and fractions

--- ReadFile.py
+++ ReadFile.py
@@ -7,19 +7,20 @@ def createSeperatedDoc(path, filename, currentContent):
 
 
 def seperateDocsInDir(path):
-    docsContent =[] #This is the Array of the content of each doc (including tags);
+    docsContent = []  # This is the Array of the content of each doc (including tags);
     for filename in os.listdir(path):
-        curFile = open(path+"\\"+filename+"\\"+filename,"r")
+        curFile = open(path + "\\" + filename + "\\" + filename, "r")
         lines = curFile.readlines()
         currentContent = []
         for line in lines:
-                currentContent.append(line)
-                if  line.__contains__("</DOC>"):
-                    docsContent.append(currentContent)
-                    return docsContent[0]
-                    #currentContent = ""
-    #print(docsContent[0])
-    #return docsContent
+            currentContent.append(line)
+            if line.__contains__("</DOC>"):
+                docsContent.append(currentContent)
+                return docsContent[0]
+                # currentContent = ""
+    # print(docsContent[0])
+    # return docsContent
+
 
 def getText():
     docText = ""
@@ -29,29 +30,25 @@ def getText():
         if line.__contains__("</TEXT>"):
             isText = False
         if isText:
-            docText+=line
+            docText += line
         if line.__contains__("<TEXT>"):
             isText = True
     return docText
 
+
 def getTerms(text):
-    terms = str.split(text," ")
-    tCount=0
+    terms = str.split(text, " ")
+    terms = filter(None, terms)
+    tCount = 0
     for term in terms:
-        terms[tCount] = re.sub('[^A-Za-z0-9\-$%/]+', '', term)
-        if terms[tCount].isdigit() == False and terms[tCount].__contains__('.') :
-            terms[tCount] = terms[tCount].replace('.', '')
-        if terms[tCount].isdigit() and terms[tCount].__contains__('.'):
-            print(terms[tCount])
-        terms[tCount] = terms[tCount].replace('\n','')
-        tCount+=1
+        terms[tCount] = re.sub('[^A-Za-z0-9\-$%/.]+', '', term)
+        if not re.match("^\d+?\.\d+?$", terms[tCount]) and not re.match(r'^\d+/\d+$', terms[tCount]):
+            terms[tCount] = re.sub('[^A-Za-z0-9\-$%]+', '', terms[tCount])
+        terms[tCount] = terms[tCount].replace('\n', '')
+        tCount += 1
     terms = filter(None, terms)
     return terms
 
 
-
-
-                    
-
-getTerms(getText())
-#seperateDocsInDir("C:\Users\USER\Desktop\SearchEngine\corpus")
+getTerms("23.4, 25.1, 3/4")
+# seperateDocsInDir("C:\Users\USER\Desktop\SearchEngine\corpus")
