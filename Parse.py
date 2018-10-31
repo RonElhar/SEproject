eb3e4bfac24fcfaac75dfae85c574b4f6174bac9
gal21 <gal.lahiani@gmail.com>
2018-10-31 17:38:55 +0200

    add treatment at fractions

--- Parse.py
+++ Parse.py
@@ -2,8 +2,9 @@ import StringIO
 import ReadFile
 import re
 
+
 def getStopWords():
-    file = open("C:\\Users\\ronel\\Desktop\\Search Engine\\SEproject\\stopwords.txt")
+    file = open("stopwords.txt")
     lines = file.readlines()
     stop_words = []
     for word in lines:
@@ -11,9 +12,10 @@ def getStopWords():
         stop_words.append(word)
     return stop_words
 
+
 class Parse:
     def __init__(self):
-     #   self.docs = ReadFile.seperateDocsInDir("D:\Studies\BGU\semesterE\IR\partA\corpus")
+        # self.docs = ReadFile.seperateDocsInDir("D:\Studies\BGU\semesterE\IR\partA\corpus")
         self.date_dict = {'JANUARY': '01', 'January': '01', 'JAN': '01', 'Jan': '01', 'FEBRUARY': '02', 'February': '02',
                      'FEB': '02', 'Feb': '02', 'MARCH': '03', 'March': '03', 'MAR': '03', 'Mar': '03', 'APRIL': '04',
                      'April': '04', 'APR': '04', 'Apr': '04', 'MAY': '05', 'May': '05', 'JUNE': '06', 'June': '06',
@@ -22,12 +24,10 @@ class Parse:
                           'Sep': '09', 'OCTOBER': '10', 'October': '10', 'OCT': '10', 'Oct': '10', 'NOVEMBER': '11',
                           'November': '11', 'NOV': '11', 'Nov': '11', 'DECEMBER': '12', 'December': '12', 'DEC': '12',
                           'Dec': '12'}
-        self.num_dict = {'Million':1000000,'million':1000000,'m':1000000,'Thousand':1000}
+        self.num_dict = {'Million': 1000000, 'million': 1000000, 'm': 1000000, 'Thousand': 1000}
         self.stop_words = getStopWords()
 
-
     def main_parser(self, list_strings):
-        length = len(list_strings)
         reg_number = re.compile(r'\d+(?:,\d*)?')
         for index, word in enumerate(list_strings):
             if self.stop_words.__contains__(word):
@@ -35,30 +35,29 @@ class Parse:
             if reg_number.match(word):
                     self.number_term(index, list_strings)
 
-    def getConvertedNumber(self,number):
-        amounts = ['','K','M','B']
+    def getConvertedNumber(self, number):
+        amounts = ['', 'K', 'M', 'B']
         counter = 0
         devider = 1
-        if number>1000000000000 :
-            (number) + amounts[counter]
-        while number>999:
-            number =number/1000
-            counter+=1
+        if number > 1000000000000:
+            number + amounts[counter]
+        while number > 999:
+            number = number / 1000
+            counter += 1
         return str(number) + amounts[counter]
 
-
-
     def number_term(self, index, list_strings):
+        length = len(list_strings)
         reg_num_with_letters = re.compile('([0-9]+[.,]?[0-9]+)([a-zA-Z]+)')
         m = reg_num_with_letters.match(list_strings[index])
         reg_num_with_dollar = re.compile('([0-9]+[.,]?[0-9]+)\$')
         m2 = reg_num_with_dollar.match(list_strings[index])
-        #check if percent
+        # Check if percent
         term = ""
         if list_strings[index + 1] == "percent" or list_strings[index + 1] == "percentage":
             term = "{}%".format(list_strings[index])
-        # check if money
-        elif m != None:
+        # Check if money
+        elif m is not None:
             if m.group(2) == "m" and list_strings[index + 1] == "Dollars":
                 term = "{} M Dollars".format(m.group(1))
             elif m.group(2) == "bn" and list_strings[index + 1] == "Dollars":
@@ -73,7 +72,7 @@ class Parse:
                     term = "{} M Dollars".format(int(float(m2.group(1))) / 1000000)
                 else:
                     term = "{} Dollars".format(m2.group(1))
-        elif list_strings[index + 2] == "U.S." and list_strings[index + 3] == "Dollars":
+        elif index + 3 < length and list_strings[index + 2] == "U.S." and list_strings[index + 3] == "dollars":
             if list_strings[index + 1] == "million":
                 term = "{} M Dollars".format(list_strings[index])
             elif list_strings[index + 1] == "billion":
@@ -81,12 +80,15 @@ class Parse:
             elif list_strings[index + 1] == "trillion":
                 term = "{} M Dollars".format(int(list_strings[index]) * 1000000)
         elif list_strings[index + 1] == "Dollars":
-            num = float(list_strings[index])
-            if num >= 1000000:
-                term = "{} M Dollars".format(int(list_strings[index]) / 1000000)
-            else:
+            if list_strings[index].__contains__("/"):
                 term = "{} Dollars".format(list_strings[index])
-        #check if Date
+            else:
+                num = float(list_strings[index])
+                if num >= 1000000:
+                    term = "{} M Dollars".format(int(list_strings[index]) / 1000000)
+                else:
+                    term = "{} Dollars".format(list_strings[index])
+        # Check if Date
         elif self.date_dict.has_key(list_strings[index + 1]) or self.date_dict.has_key(list_strings[index - 1]):
             month = ""
             if self.date_dict.has_key(list_strings[index + 1]):
@@ -102,7 +104,7 @@ class Parse:
                 term = "{}-{}".format(list_strings[index], month)
         else:
             if list_strings[index + 1] == "Thousand":
-                term = "{}".format(list_strings[index],'K')
+                term = "{}".format(list_strings[index], 'K')
             elif list_strings[index + 1] == "Million":
                 term = "{}".format(list_strings[index], 'M')
             elif list_strings[index + 1] == "Billion":
@@ -121,4 +123,5 @@ class Parse:
 
 parse = Parse()
 parse.main_parser(["May", "1994", "14", "MAY", "JUNE", "4", "20.6bn", "Dollars", "32bn", "Dollars", "1000", "Dollars",
-                   "40.5", "Dollars", "100", "billion", "U.S.", "Dollars", "320", "million", "U.S.", "Dollars","1000.23", "1", "trillion", "U.S.", "Dollars"])
+                   "40.5", "Dollars", "100", "billion", "U.S.", "dollars", "320", "million", "U.S.", "dollars", "1",
+                   "trillion", "U.S.", "dollars", "22 3/4", "Dollars"])
