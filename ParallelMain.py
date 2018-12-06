56ae5f3dc0b86b62e2340733b53548d45a3eb67a
ronelhar <ronelharrar@gmail.com>
2018-12-06 23:24:28 +0200

    Fixed path

--- ParallelMain.py
+++ ParallelMain.py
@@ -97,30 +97,30 @@ def get_corpus_4partition(dirs_list, main_path):
 """
 
 
-def start(corpus_path, posting_path, to_stem, dirs_list):
-    files_partition = get_corpus_4partition(dirs_list, corpus_path)
+def start(main_path, posting_path, to_stem, dirs_list):
+    files_partition = get_corpus_4partition(dirs_list, main_path + '\\corpus')
     manager = multiprocessing.Manager()
     dirs_dicts = manager.dict()
     p1 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, corpus_path, posting_path, to_stem, 0, files_partition[0],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 0, files_partition[0],
                                      "\\Postings1"))
 
     p2 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, corpus_path, posting_path, to_stem, files_partition[0],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[0],
                                      files_partition[1],
                                      "\\Postings2"))
 
     p3 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, corpus_path, posting_path, to_stem, files_partition[1],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[1],
                                      files_partition[2],
                                      "\\Postings3"))
 
     p4 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, corpus_path, posting_path, to_stem, files_partition[2],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[2],
                                      len(dirs_list), "\\Postings4"))
 
     p1.start()
