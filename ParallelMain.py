3940ef8a719b7cdc260ce5b44e2797be6aa9d010
RonElhar <ronelharrar@gmail.com>
2018-12-21 08:43:07 +0200

    added avg len, read terms in searcher

--- ParallelMain.py
+++ ParallelMain.py
@@ -103,25 +103,25 @@ def start(main_path, posting_path, to_stem, dirs_list):
     dirs_dicts = manager.dict()
     p1 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 0, files_partition[0],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 0, 10,
                                      "\\Postings1"))
 
     p2 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[0],
-                                     files_partition[1],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 20,
+                                     30,
                                      "\\Postings2"))
 
     p3 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[1],
-                                     files_partition[2],
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 20,
+                                     30,
                                      "\\Postings3"))
 
     p4 = multiprocessing.Process(target=start_indexing,
                                  args=(
-                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, files_partition[2],
-                                     len(dirs_list), "\\Postings4"))
+                                     dirs_list, dirs_dicts, main_path, posting_path, to_stem, 30,
+                                     40, "\\Postings4"))
 
     p1.start()
     p2.start()
